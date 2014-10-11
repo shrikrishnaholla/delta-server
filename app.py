50637520066d5f8078bb94827a9def8665ec760d
Phalgun <pg@getsigneasy.com>
2014-10-11 22:25:03 +0530

    Recommendations based on bucket completed

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask.ext import restful
 from flask.ext.restful import reqparse
 import redis
+import json
+import ast
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -21,8 +23,7 @@ class UserList(restful.Resource):
 
         parser = reqparse.RequestParser()
         parser.add_argument('email', type = str, required = True, location = 'form')
-        parser.add_argument('first_name', required = True, location = 'form')
-        parser.add_argument('title', required = True, location = 'form')
+        parser.add_argument('data', type = str, required = True, location = 'form')
 
         args = parser.parse_args()
 
@@ -36,9 +37,9 @@ class UserList(restful.Resource):
             r.set(user_email, user_data)
             return {'email': user_email, 'data': r.get(user_email)}, 201
 
-class Reco(restful.Resource):
+class RandReco(restful.Resource):
     def get(self, email):
-        r = redis.StrictRedis(host = 'localhost', port = 6379, db=0)
+        r = redis.StrictRedis(host = 'localhost', port = 6379, db = 0)
         if r.exists(email):
             rand_user = r.randomkey()
             while (rand_user == email):
@@ -47,8 +48,61 @@ class Reco(restful.Resource):
         else:
             return {'message': 'User does not exist'}, 400
 
+class Reco(restful.Resource):
+
+    def algorithm(self, user_email):
+        try:
+            # get data
+            r = redis.StrictRedis(host = 'localhost', port = 6379, db = 0)
+            user_data = r.get(user_email)
+            print user_data
+            print ast.literal_eval(user_data)
+
+            # convert data to json
+            user_data_json = json.loads(user_data)
+            print 'created user_data_json'
+            # get another random person
+            for x in range(1, 30):
+                rand_user_email = r.randomkey()
+                print 'got rand_user_email' + str(rand_user_email)
+                if rand_user_email == user_email:
+                    continue
+                print r.get(rand_user_email)
+                rand_user_data = r.get(rand_user_email)
+                rand_user_data_json = json.loads(rand_user_data)
+
+                # if user and reco_user belong to the same bucket, do another rand
+                if rand_user_data_json.get('bucket') is None:
+                    continue
+                if user_data_json.get('bucket') is None:
+                    continue
+                if rand_user_data_json['bucket'] == user_data_json['bucket']:
+                    continue
+
+                # if user and reco_user are connected, continue
+                # TODO
+
+                return rand_user_email
+            return None
+        except Exception, e:
+            raise
+            print e
+            print user_email
+            return None
+
+    def get(self, email):
+        r = redis.StrictRedis(host = 'localhost', port = 6379, db = 0)
+        if r.exists(email):
+            reco_user_email = self.algorithm(email)
+            if reco_user_email is None:
+                return 500
+            return {'reco_user_email': reco_user_email, 'data': r.get(reco_user_email)}, 200
+        else:
+            return {'message': 'User does not exist'}, 400
+
 api.add_resource(User, '/user/<string:email>')
 api.add_resource(UserList, '/user')
+api.add_resource(RandReco, '/user/<string:email>/rand')
 api.add_resource(Reco, '/user/<string:email>/reco')
 
 if __name__ == '__main__':
