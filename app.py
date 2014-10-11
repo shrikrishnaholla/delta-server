e98ad8defe57b9cc06fb724ff6c6a4c8619764da
Phalgun <pg@getsigneasy.com>
2014-10-11 14:59:45 +0530

    Implemented GET /user/<email_id>, POST /user, GET /user/<email_id>/reco

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+from flask.ext import restful
+from flask.ext.restful import reqparse
+import redis
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class User(restful.Resource):
+    def get(self, email):
+        r = redis.StrictRedis(host = 'localhost', port = 6379, db=0)
+        if r.exists(email):
+            return {'email': email, 'data': r.get(email)}
+        else:
+            return {}, 404
+
+class UserList(restful.Resource):
+    def post(self, email = None):
+        if email is not None:
+            restful.abort()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type = str, required = True, location = 'form')
+        parser.add_argument('data', required = True, location = 'form')
+        args = parser.parse_args()
+
+        user_email = args.get('email')
+        user_data = args.get('data')
+
+        r = redis.StrictRedis(host = 'localhost', port = 6379, db=0)
+        if r.exists(user_email):
+            return {'message':'User already exists'}, 200
+        else:
+            r.set(user_email, user_data)
+            return {'email': user_email, 'data': r.get(user_email)}, 201
+
+class Reco(restful.Resource):
+    def get(self, email):
+        r = redis.StrictRedis(host = 'localhost', port = 6379, db=0)
+        if r.exists(email):
+            rand_user = r.randomkey()
+            print rand_user
+            while (rand_user == email):
+                rand_user = r.randomkey()
+            return {'email': rand_user, 'data': r.get(rand_user)}, 200
+        else:
+            return {'message': 'User does not exist'}, 400
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(User, '/user/<string:email>')
+api.add_resource(UserList, '/user')
+api.add_resource(Reco, '/user/<string:email>/reco')
+
+if __name__ == '__main__':
+    app.run(debug=True)
